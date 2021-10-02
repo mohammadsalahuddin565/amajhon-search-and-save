2d5a1e9e20d10dabf5b8015b661c4b9b6e417cff
mohammadsalahuddin565 <mohammadsalahuddin565@gmail.com>
2021-10-02 21:52:39 +0600

    updated push

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -4,6 +4,7 @@ const Inventory = () => {
     return (
         <div>
             <h2>This is Inventroy component and git set remote url origin</h2>
+            <h3>this is updated</h3>
         </div>
     );
 };
